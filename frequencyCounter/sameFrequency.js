a1adddf3c537a55aaa51f292296a227b0930b7e8
Erick <erickchhuang@gmail.com>
2025-04-04 16:08:20 -0700

    Update sameFrequency.js

--- frequencyCounter/sameFrequency.js
+++ frequencyCounter/sameFrequency.js
@@ -1,5 +1,4 @@
 function sameFrequency(a,b){
-    // good luck. Add any arguments you deem necessary.
     let num1 = a.toString();
     let num2 = b.toString();
     let hash = {};
@@ -20,4 +19,3 @@ function sameFrequency(a,b){
     return true;
     }
   };
-  
