882723d1d82e9cec144b5d28fecf8d8abf6c0960
Erick <erickchhuang@gmail.com>
2025-04-10 17:38:43 -0700

    Update selectionSort.js

--- searchAlgorithms/selectionSort/selectionSort.js
+++ searchAlgorithms/selectionSort/selectionSort.js
@@ -6,7 +6,6 @@ function selectionSort(arr) {
     // if the min is not the value (index) you began with, swap the two values;
     // repeat this with the next element until the array is sorted;
     for (let i = 0; i < arr.length; i++) {
-        let lowest = i;
         for(let j = i + 1; j < arr.length; j++) {
             if(arr[j] < arr[lowest]) {
                 lowest = j;
