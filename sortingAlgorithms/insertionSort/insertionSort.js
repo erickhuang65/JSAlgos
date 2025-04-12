95221986e33ae29ae91ba23c04778284dcd4c033
Erick <erickchhuang@gmail.com>
2025-04-11 17:45:31 -0700

    algos

--- sortingAlgorithms/insertionSort/insertionSort.js
+++ sortingAlgorithms/insertionSort/insertionSort.js
@@ -8,8 +8,15 @@
 // repeat until the array is sorted 
 
 function insertionSort(arr) {
-    
+    for(let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentVal;
+        console.log(arr);
+    }
     return arr;
 }
 
-console.log(insertionSort([10, 50, 2, 10, 8]))
+console.log(insertionSort([3, 50, 5, 10, 8, 15]))
