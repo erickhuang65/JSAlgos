42da002564d8e66fc9ed5e67f51c4993e43acc4d
Erick <erickchhuang@gmail.com>
2025-04-14 23:00:38 -0700

    Update doublyLinkedList.js

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -13,14 +13,7 @@ class DoublyLinkedList {
         this.length = 0;
     }
 
-    // Push pesudocode:
-    // create a new node with the value passed to the function
-    // if the head is null; set the head and tail to be the newly created node
-    // if not, set the next property on the tail to be that node
-    // set the previous property on the newly created node to be the tail
-    // set the tail to be the newly created node
-    // extend the length;
-    // return the DLL
+
     push(val) {
         var newNode = new Node(val);
         if(this.head === null) {
