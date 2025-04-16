3d8ceb02ddb90329c449b709fd26d21f203f39d2
Erick <erickchhuang@gmail.com>
2025-04-16 15:30:02 -0700

    Update stacks.js

--- dataStructures/stacks.js
+++ dataStructures/stacks.js
@@ -1,24 +1,33 @@
 class Node {
     constructor(val) {
         this.val = val;
-        this.prev = null;
         this.next = null;
     }
 }
 
 class Stack{
     constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+        this.first = null;
+        this.last = null;
+        this.size = 0;
     }
 
     push(val){
         // create a new node with the argument
+        var newNode = new Node(val);
         // if there are no nodes in the stack, set the first and last to the new node
-        // if there is at least one node, create a variable that stores the current property on the stack
-        // reset the first property to the newly created node
-        // set the next property on the node to be the previous created var
-        // increment the size of the stack by 1;
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            // if there is at least one node, create a variable that stores the current property on the stack
+            // reset the first property to the newly created node
+            // set the next property on the node to be the previous created var
+            var temp  = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+            // increment the size of the stack by 1;
+            return ++this.size;
+        }
     }
 }
