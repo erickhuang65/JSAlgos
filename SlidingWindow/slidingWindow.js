3193493d136e4d3e04dccb18247936ce6a75d279
Erick <erickchhuang@gmail.com>
2025-04-19 10:55:51 -0700

    Update slidingWindow.js

--- SlidingWindow/slidingWindow.js
+++ SlidingWindow/slidingWindow.js
@@ -1,6 +1,6 @@
  // pattern involves:
 // creating window which can either be an array or number from one position to another
-// useful for keeping tarck of a subset of data in an array/string
+// useful for keeping track of a subset of data in an array/string
 
 // accepts an array of integers and a number called n
 // calculate the max sum of n consecutive elements in the array
