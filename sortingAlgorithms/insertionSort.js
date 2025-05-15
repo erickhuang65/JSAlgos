76c7788784edbf343c5c10286716bd163b01e94c
Erick <erickchhuang@gmail.com>
2025-05-14 21:03:55 -0700

    Update insertionSort.js

--- sortingAlgorithms/insertionSort.js
+++ sortingAlgorithms/insertionSort.js
@@ -1,11 +1,3 @@
-// taking an element from the array at a time
-// sort it to the left side of the array
-// will first consider the first element is sorted
-
-// start by picking the second element in the array
-// compare the second element with the one before; swap if necessary
-// continue to the next. if it is in the incorrect order, iterate through the sorted portion (e.g. left side)
-// repeat until the array is sorted 
 
 function insertionSort(arr) {
     for(let i = 1; i < arr.length; i++) {
