d2a15ce94997b7e86db8298824ed87a251f3ceae
Erick <erickchhuang@gmail.com>
2025-05-30 16:18:19 -0700

    Adding mergedAlternatively

---
+++ leetCode/mergedAlternatively.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    // loop through the entire word1;
+    let mergedWord = "";
+    if(word1.length > word2.length) {
+        for(let i = 0; i < word1.length; i++) {
+            mergedWord += word1[i];
+            mergedWord += word2[i];
+        }
+    } else {
+        for(let i = 0; i < word2.length; i++) {
+            mergedWord += word1[i];
+            mergedWord += word2[i];
+        }
+    }
+    let cleaned = mergedWord.replace(/undefined/g, "");
+    return cleaned; 
+};
