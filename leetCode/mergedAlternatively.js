2b357ef185a36b38ce2ab046eb9ce65d5661b0c1
Erick <erickchhuang@gmail.com>
2025-05-30 16:25:03 -0700

    revised mergedAlternatively

--- leetCode/mergedAlternatively.js
+++ leetCode/mergedAlternatively.js
@@ -3,20 +3,30 @@
  * @param {string} word2
  * @return {string}
  */
+// var mergeAlternately = function(word1, word2) {
+//     // loop through the entire word1;
+//     let mergedWord = "";
+//     if(word1.length > word2.length) {
+//         for(let i = 0; i < word1.length; i++) {
+//             mergedWord += word1[i];
+//             mergedWord += word2[i];
+//         }
+//     } else {
+//         for(let i = 0; i < word2.length; i++) {
+//             mergedWord += word1[i];
+//             mergedWord += word2[i];
+//         }
+//     }
+//     let cleaned = mergedWord.replace(/undefined/g, "");
+//     return cleaned; 
+// };
 var mergeAlternately = function(word1, word2) {
-    // loop through the entire word1;
-    let mergedWord = "";
-    if(word1.length > word2.length) {
-        for(let i = 0; i < word1.length; i++) {
-            mergedWord += word1[i];
-            mergedWord += word2[i];
-        }
-    } else {
-        for(let i = 0; i < word2.length; i++) {
-            mergedWord += word1[i];
-            mergedWord += word2[i];
-        }
+    let i = 0;
+    let mergedArr = [];
+    while(i < word1.length || i < word2.length) {
+        mergedArr.push(word1[i]);
+        mergedArr.push(word2[i]);
+        i++;
     }
-    let cleaned = mergedWord.replace(/undefined/g, "");
-    return cleaned; 
+    return mergedArr.join('');
 };
